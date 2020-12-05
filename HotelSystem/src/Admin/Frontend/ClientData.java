82681bf7d359a4b7d375ab8dd187b89c10868a61
LoukHin <loukhin3141@gmail.com>
2020-12-05 15:03:06 +0700

    Set look & feel

--- HotelSystem/src/Admin/Frontend/ClientData.java
+++ HotelSystem/src/Admin/Frontend/ClientData.java
@@ -47,11 +47,11 @@ public class ClientData extends javax.swing.JFrame {
         }
 
         // data of the table
-        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        Vector<Vector<String>> data = new Vector<Vector<String>>();
         while (rs.next()) {
-            Vector<Object> vector = new Vector<Object>();
+            Vector<String> vector = new Vector<String>();
             for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                vector.add(rs.getObject(columnIndex));
+                vector.add(rs.getString(columnIndex));
             }
             data.add(vector);
         }
@@ -149,35 +149,7 @@ public class ClientData extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ClientData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ClientData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ClientData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ClientData.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ClientData().setVisible(true);
-            }
-        });
+        new ClientData().setVisible(true);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
