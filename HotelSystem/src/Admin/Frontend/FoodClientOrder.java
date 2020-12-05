93333545016d91c7d5f71429bd6f6c9d45ae57d9
Arpo <arpo555@hotmail.com>
2020-12-05 14:19:44 +0700

    FoodClientOrder set variable to static and space on GUI

--- HotelSystem/src/Admin/Frontend/FoodClientOrder.java
+++ HotelSystem/src/Admin/Frontend/FoodClientOrder.java
@@ -107,7 +107,7 @@ public class FoodClientOrder extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(0, 0, 0)
                 .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
