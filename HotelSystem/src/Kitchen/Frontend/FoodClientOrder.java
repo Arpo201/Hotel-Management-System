d9c130d572d5c5678cb744ee84531f245aff6863
Arpo <arpo555@hotmail.com>
2020-12-08 21:07:57 +0700

    FoodClientOrder delete testBtn

--- HotelSystem/src/Kitchen/Frontend/FoodClientOrder.java
+++ HotelSystem/src/Kitchen/Frontend/FoodClientOrder.java
@@ -37,89 +37,78 @@ public class FoodClientOrder extends JFrame implements WindowListener {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        buttonGroup1 = new ButtonGroup();
-        jPanel1 = new JPanel();
-        headLabel = new JLabel();
-        body = new JPanel();
-        desktopPane = new JDesktopPane();
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jPanel1 = new javax.swing.JPanel();
+        headLabel = new javax.swing.JLabel();
+        body = new javax.swing.JPanel();
+        desktopPane = new javax.swing.JDesktopPane();
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(67, 106, 141));
 
         headLabel.setFont(new java.awt.Font("Angsana New", 1, 48)); // NOI18N
         headLabel.setForeground(new java.awt.Color(255, 255, 255));
-        headLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        headLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         headLabel.setText("Order");
 
-        GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addComponent(headLabel, GroupLayout.Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(headLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addComponent(headLabel, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(headLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         body.setBackground(new java.awt.Color(234, 242, 249));
 
         desktopPane.setBackground(new java.awt.Color(227, 234, 240));
 
-
-        GroupLayout desktopPaneLayout = new GroupLayout(desktopPane);
+        javax.swing.GroupLayout desktopPaneLayout = new javax.swing.GroupLayout(desktopPane);
         desktopPane.setLayout(desktopPaneLayout);
         desktopPaneLayout.setHorizontalGroup(
-            desktopPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(GroupLayout.Alignment.TRAILING, desktopPaneLayout.createSequentialGroup()
-                .addContainerGap(928, Short.MAX_VALUE)
-                .addContainerGap())
+            desktopPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1019, Short.MAX_VALUE)
         );
         desktopPaneLayout.setVerticalGroup(
-            desktopPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(desktopPaneLayout.createSequentialGroup()
-                .addContainerGap()
-                .addContainerGap(481, Short.MAX_VALUE))
+            desktopPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 519, Short.MAX_VALUE)
         );
 
-        GroupLayout bodyLayout = new GroupLayout(body);
+        javax.swing.GroupLayout bodyLayout = new javax.swing.GroupLayout(body);
         body.setLayout(bodyLayout);
         bodyLayout.setHorizontalGroup(
-            bodyLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(desktopPane)
         );
         bodyLayout.setVerticalGroup(
-            bodyLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(GroupLayout.Alignment.TRAILING, bodyLayout.createSequentialGroup()
+            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bodyLayout.createSequentialGroup()
                 .addGap(0, 0, 0)
                 .addComponent(desktopPane)
                 .addGap(0, 0, 0))
         );
 
-        GroupLayout layout = new GroupLayout(getContentPane());
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(body, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(body, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void testBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_testBtnActionPerformed
-        desktopPane.add(new FoodOrderForm());
-//        addOrder(new JSONObject());
-        System.out.println("add");
-    }//GEN-LAST:event_testBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -134,11 +123,11 @@ public class FoodClientOrder extends JFrame implements WindowListener {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private JPanel body;
-    private ButtonGroup buttonGroup1;
-    private static JDesktopPane desktopPane = new JDesktopPane();
-    private JLabel headLabel;
-    private JPanel jPanel1;
+    private javax.swing.JPanel body;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private static javax.swing.JDesktopPane desktopPane;
+    private javax.swing.JLabel headLabel;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 
     @Override
